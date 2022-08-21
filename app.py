36921d3ec90fd672c76e4514f991180d24617f52
Volodymir Pugachov <volodymir.nsgroup@gmail.com>
2022-08-21 21:30:26 +0300

    broken

--- app.py
+++ app.py
@@ -9,8 +9,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("My App")
-        button = QPushButton("Press Me!")
+        
 
         # Set the central widget of the Window.
         self.setCentralWidget(button)
